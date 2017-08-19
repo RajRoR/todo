b6d8dfa616a9db311a9c38778fa8c291ae837165
Raj <raj_abc38@yahoo.com>
2017-08-19 19:17:57 +0530

    Restart resque after deploy

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,4 +1,4 @@
 if config.current_name == 'resque'
-  sudo "cd #{config.release_path} monit restart resque_todo_0"
-  sudo "cd #{config.release_path} monit restart resque_todo_1"
+  sudo "cd #{config.release_path} monit restart resque_todo"
+  # sudo "cd #{config.release_path} monit restart resque_todo"
 end
